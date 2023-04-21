c73545d8b7047e2730efdab74b8f4a67d6a2d6bd
Paola Danielle Ferrete <pferrete@c2r8s3.42lisboa.com>
2023-04-21 20:06:08 +0100

    add changes

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pferrete <pferrete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 11:20:14 by pferrete          #+#    #+#             */
-/*   Updated: 2023/04/19 11:22:43 by pferrete         ###   ########.fr       */
+/*   Updated: 2023/04/21 20:05:28 by pferrete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,17 @@ size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 {
 	size_t	slen;
 	size_t	dpos;
+	size_t	dlen;
 	int		i;
 
-	slen = 0;
-	dpos = 0;
+	slen = strlen(src);
+	dlen = strlen(dest);
+	dpos = dlen;
 	i = 0;
-	while (src[slen] != '\0')
-	{
-		slen++;
-	}
-	while (dest[dpos] != '\0' && dpos < dstsize)
-	{
-		dpos++;
-	}
-	while (src[i] != '\0' && dpos < dstsize)
+	while (src[i] != '\0' && dpos < dstsize - 1)
 	{
 		dest[dpos++] = src[i++];
 	}
-	if (dpos < dstsize)
-	{
-		dest[dpos] = '\0';
-	}
-	return (dpos);
+	dest[dpos] = '\0';
+	return (dlen + dpos - 1);
 }
