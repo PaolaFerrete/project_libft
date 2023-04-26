9b821374be2ef0a8ba46949479081cb298e493ec
Paola Danielle Ferrete <pferrete@c2r8s3.42lisboa.com>
2023-04-26 10:07:54 +0100

    add ft_split

--- ft_split.c
+++ ft_split.c
@@ -1,39 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pferrete <pferrete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/24 12:30:01 by pferrete          #+#    #+#             */
-/*   Updated: 2023/04/26 09:15:55 by pferrete         ###   ########.fr       */
+/*   Created: 2023/04/26 09:21:46 by pferrete          #+#    #+#             */
+/*   Updated: 2023/04/26 09:28:14 by pferrete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*create a new string which is a duplicate of the string s*/
-
-char	*ft_strdup(const char *s)
+char	**ft_split(char const *s, char c)
 {
-	char	*cstring;
-	size_t	i;
+	char	*spt;
 
 	if (!s)
-	{
 		return (NULL);
-	}
-	i = 0;
-	cstring = (char *)malloc(sizeof(*s) * (ft_strlen(s) + 1));
-	if (!cstring)
-	{
-		return (NULL);
-	}
-	while (s[i])
-	{
-		cstring[i] = s[i];
-		i++;
-	}
-	cstring[i] = '\0';
-	return (cstring);
+
 }
