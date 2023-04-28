6e5dcc730285ac87fc2ad1233e841006577792d9
Paola Danielle Ferrete <pferrete@c2r8s3.42lisboa.com>
2023-04-28 14:05:10 +0100

    add ft_putstr_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pferrete <pferrete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 10:29:09 by pferrete          #+#    #+#             */
-/*   Updated: 2023/04/28 13:32:44 by pferrete         ###   ########.fr       */
+/*   Updated: 2023/04/28 14:04:16 by pferrete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,5 +84,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 /*it's a function void */
 void	ft_putchar_fd(char c, int fd);
+/*it's a function void*/
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
